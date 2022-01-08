e1d55002a806c268726429d27a0515b314f8e6f1
Sujit <sjoshi1@saic.edu>
2022-01-08 11:00:32 +0530

    c

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function clientConnectForm(){
+    window.open('https://airtable.com/shrU3JNmuxsah2Pw9');
+}
+
+function jobUpdateForm() {
+    window.open('https://airtable.com/shr67jSev4hSdCbcG');
+}
+
+function newJobForm() {
+    window.open('https://airtable.com/shrANy3rDlBoMiKgK');
+}
+
+function newClientForm() {
+    window.open('https://airtable.com/shrf0Ueb2uThl4DxQ');
+}
