f67d0a0a75014b769f331d262e7462cca3336feb
Sujit <sjoshi1@saic.edu>
2023-07-27 19:55:23 +0530

    c

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 
-function clientConnectForm(){
-    window.open('https://airtable.com/shrU3JNmuxsah2Pw9');
-}
+// function clientConnectForm(){
+    // window.open('https://airtable.com/shrU3JNmuxsah2Pw9');
+// }
 
 function jobUpdateForm() {
-    window.open('https://airtable.com/shr67jSev4hSdCbcG');
+    window.open('https://airtable.com/appe5vnQC7Rh19mee/shrYL5rtrtBiUV0vO');
 }
 
 function newJobForm() {
-    window.open('https://airtable.com/shrANy3rDlBoMiKgK');
+    window.open('https://airtable.com/appe5vnQC7Rh19mee/shrdDQuwuozpTcO32');
 }
 
-function newClientForm() {
-    window.open('https://airtable.com/shrf0Ueb2uThl4DxQ');
-}
-function viewClientDiscussion() {
-    window.open('https://airtable.com/shrTW0tuTCDPTsCVH');
-}
+// function newClientForm() {
+    // window.open('https://airtable.com/shrf0Ueb2uThl4DxQ');
+// }
+// function viewClientDiscussion() {
+    // window.open('https://airtable.com/shrTW0tuTCDPTsCVH');
+// }
 function viewAllOrders() {
-    window.open('https://airtable.com/shr3Dd15J94gnNPTA');
+    window.open('https://airtable.com/appe5vnQC7Rh19mee/shr7qkvboHyfK9spZ');
 }
-function viewJobStage() {
-    window.open('https://airtable.com/shr21dP7c3HpcjCTk');
-}
-function viewPendingJobs() {
-    window.open('https://airtable.com/shruqtzzNhCwzLtRj/tbloW6vFGkUAt13ey');
-}
+// function viewJobStage() {
+    // window.open('https://airtable.com/shr21dP7c3HpcjCTk');
+// }
+// function viewPendingJobs() {
+    // window.open('https://airtable.com/shruqtzzNhCwzLtRj/tbloW6vFGkUAt13ey');
+// }
